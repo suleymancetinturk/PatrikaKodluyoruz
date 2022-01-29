e9fcf34acb1a6d901f7894c38ea7291250af090a
Süleyman Çetintürk <suleymancetinturk32@gmail.com>
2022-01-29 20:59:50 +0300

    C# 101 ilk Pratik

---
+++ C# 101/Pratikler/console programlama/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace console_programlama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Adınız...");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyadınız...");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Sayın" +" " + name + " " + surname);
+        }
+    }
+}
