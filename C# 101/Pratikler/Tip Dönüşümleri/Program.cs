a137dda471139fd85362c824ff59b13a138f0d7f
Süleyman Çetintürk <suleymancetinturk32@gmail.com>
2022-01-31 02:16:38 +0300

    Tip Dönüşümleri

---
+++ C# 101/Pratikler/Tip Dönüşümleri/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace Tip_Dönüşümleri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // implicit conversion (Bilinçsiz Dönüşüm)
+            byte a = 5;
+            sbyte b = 30;
+            short c = 10;
+
+            int d = a + b + c;
+            Console.WriteLine("d:" + d);
+
+
+            long h = d;
+            Console.WriteLine("h:" + h);
+
+            float i = h;
+            Console.WriteLine("i:" + i);
+
+            string e = "süleyman";
+            char f = 'k';
+            object g = e + f + d;
+            Console.WriteLine("g:" + g);
+
+
+            // explicit conversion (Bilinçli Dönüşüm)
+            Console.WriteLine("***Explicit Conversion***");
+            int x = 4;
+            byte y = (byte)x;
+            Console.WriteLine("y:" + y);
+
+            int z = 4;
+            byte t = (byte)z;
+            Console.WriteLine("t:" + t);
+
+
+            float w = 10.3f;
+            byte v = (byte)w;
+            Console.WriteLine("v:" + v);
+
+            //***Tostring metedu ***
+            Console.WriteLine("***Tostring metedu ***");
+            int xx = 6;
+            string yy = xx.ToString();
+            Console.WriteLine("y:" + yy);
+
+            string zz = 12.5f.ToString();
+            Console.WriteLine("zz:" + zz);
+
+
+            //system.Convert
+            Console.WriteLine("**system.Convert Sınıfı**");
+            string s1 = "10", s2 = "20";
+            int sayi1, sayi2;
+            int toplam;
+
+            sayi1 = Convert.ToInt32(s1);
+            sayi2 = Convert.ToInt32(s2);
+            toplam = sayi1 + sayi2;
+            Console.WriteLine("toplam:" + toplam);
+
+            // Parse 
+            Console.WriteLine("** Parse Methodu **");
+            ParseMethod();
+        }
+        public static void ParseMethod()
+        {
+            string metin1 = "10";
+            string metin2 = "10,25";
+            int rakam1;
+            double double1;
+
+            rakam1 = Int32.Parse(metin1);
+            double1 = Double.Parse(metin2);
+            Console.WriteLine("rakam1:" + rakam1);
+            Console.WriteLine("double1:" + double1);
+
+
+        }
+    }
+}
